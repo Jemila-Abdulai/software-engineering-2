0fb92de919030c4304731eadbad68ef116db2ab5
Jemila-Abdulai <56167159+Jemila-Abdulai@users.noreply.github.com>
2022-09-22 10:52:35 +0100

    Update app.py

--- soil-moisture-sensor/app.py
+++ soil-moisture-sensor/app.py
@@ -2,7 +2,7 @@ from counterfit_connection import CounterFitConnection
 CounterFitConnection.init('127.0.0.1', 5000)
 
 import time
-    from counterfit_shims_grove.adc import ADC
+from counterfit_shims_grove.adc import ADC
 from counterfit_shims_grove.grove_relay import GroveRelay
 import json
 from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
