f4f22d6d726ed4bd35b8c530d750e87d055da472
Christopher Lloyd <Christopher.Lloyd2@ibm.com>
2022-10-13 11:35:05 +0100

    fix tests

--- dashboard/src/components/App/App.jsx
+++ dashboard/src/components/App/App.jsx
@@ -1,46 +1,43 @@
 import React from "react"
-import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 
 // The best way to write React applications is to split your App into separate components.
 // I would recommend reading this https://reactjs.org/tutorial/tutorial.html#overview
 
 export default class App extends React.Component {
-    constructor () {
+    constructor() {
         super()
         this.state = {
             devices: []
         }
     }
 
-    componentDidMount () {
+    componentDidMount() {
         this.getDevices()
     }
 
-    async getDevices () {
+    async getDevices() {
         const res = await fetch('/api/devices')
-        this.setState({ devices: await res.json() })
+        const devices = await res.json()
+        this.setState({ devices })
     }
 
-    render () {
+    render() {
         const devices = this.state.devices
         return (
             <>
-                { 
-                    devices.map( device => 
-                        <Card style={{ width: '18rem' }} bg={ device.alive ? 'success' : 'danger'}>
-                            
-                        <Card.Body>
-                            <Card.Title> {device.device_name}</Card.Title>
-                            <Card.Text>ID:{device.id}</Card.Text>
-                        </Card.Body>
-                        </Card> 
-                    ) 
-                    
+                {
+                    devices.map(device =>
+                        <Card style={{ width: '18rem' }} bg={device.alive ? 'success' : 'danger'} key={device.id}>
+                            <Card.Body>
+                                <Card.Title> {device.device_name}</Card.Title>
+                                <Card.Text>ID:{device.id}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    )
                 }
-                
             </>
-            
+
         )
     }
 }
