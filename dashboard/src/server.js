5b5bab82fe77a282c5f8a166dd0fd50c13c69190
Christopher Lloyd <Christopher.Lloyd2@ibm.com>
2022-10-20 20:43:48 +0100

    connect to the real iot hub

--- dashboard/src/server.js
+++ dashboard/src/server.js
@@ -1,24 +1,62 @@
+// Load environment variables from .env file
+require('dotenv').config()
+
 const path = require('path')
 const express = require('express')
-const app = express()
-const port = 8081
+const iothub = require('azure-iothub')
 
-const root = path.join(__dirname, '../', '../')
-const mock_data = [
-  { id: 5673, device_name: 'soil_sensor_1', alive: true },
-  { id: 5612, device_name: 'soil_sensor_2', alive: true },
-  { id: 5243, device_name: 'soil_sensor_3', alive: false }
+const inProd = !process.env.DEV
+
+const requiredEnvVars = [
+  'IOT_CONN_STR'
 ]
 
+const config = {
+  IOT_CONN_STR: process.env.IOTHUB_CONN_STR,
+  PORT: process.env.PORT || 8080
+}
+
+requiredEnvVars.forEach(envVar => {
+  if (config[envVar] === undefined) {
+    throw Error(`Missing ${envVar} environment variable`)
+  }
+})
+
+let devices = [
+  { id: 'my-device1', alive: true },
+  { id: 'my-device2', alive: false }
+]
+
+if (!inProd) {
+  devices = [
+    { id: 'my-device1', alive: true },
+    { id: 'my-device2', alive: false }
+  ]
+} else {
+  const iothubRegistry = iothub.Registry.fromConnectionString(config.IOT_CONN_STR)
+  setInterval(async () => {
+    const res = await iothubRegistry.list()
+    devices = res.responseBody
+  }, 1000)
+}
+
+// Setup HTTP Server
+const app = express()
 app.use(express.json())
 
-// Serve the build
+const root = path.join(__dirname, '../', '../')
 app.use('/', express.static(path.join(root, 'dist')))
 
 app.get('/api/devices', (req, res) => {
-  res.json(mock_data)
+  const deviceList = devices.map(device => {
+    return {
+      id: device.deviceId,
+      alive: device.connectionState === 'Connected'
+    }
+  })
+  res.json(deviceList)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(config.PORT, () => {
+  console.log(`Example app listening on port ${config.PORT}`)
 })
