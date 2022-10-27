92f8370d33163648d69fae25a56951e5cf2fbbf0
ashketchupppp <computerchris101@gmail.com>
2022-10-27 12:42:50 +0100

    remove comments

--- util/rollback.sh
+++ util/rollback.sh
@@ -6,10 +6,3 @@ if [[ -n $TAG ]]; then
 else
   echo "Missing argument: TAG"
 fi
-
-crap code is now in main
-
-on a branch
-git revert to a tag which isn't broken, most recent
-merge the branch
-delete the new crap tag from master
