9246748b4314099b26dd02f1c500a809cc68c31a
ashketchupppp <computerchris101@gmail.com>
2022-10-27 11:44:39 +0100

    update

--- util/bump-version.sh
+++ util/bump-version.sh
@@ -15,7 +15,4 @@ then
 
   # bump the version
   python -m bumpversion $1 --config-file VERSION.cfg
-  VERSION=$(python util/what-version-are-we.py)
-  git tag -a $VERSION -m "Bump to $VERSION"
-  git push origin $VERSION
 fi
