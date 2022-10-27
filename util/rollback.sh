38e40b28135641230d7790400f279cd1004b41f7
ashketchupppp <computerchris101@gmail.com>
2022-10-27 12:38:14 +0100

    readme and rollback

--- util/rollback.sh
+++ util/rollback.sh
@@ -1,7 +1,15 @@
 TAG=$1
 
 if [[ -n $TAG ]]; then
+  git checkout -b rollback-$TAG
   git revert $TAG
 else
   echo "Missing argument: TAG"
 fi
+
+crap code is now in main
+
+on a branch
+git revert to a tag which isn't broken, most recent
+merge the branch
+delete the new crap tag from master
